f14b757e44464d57a3a4f8f98c263f9e9f4a4478
DNSCond <condi7002@gmail.com>
2026-05-11 12:06:02 +0200

    hello

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,42 +1,16 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
-import { context, createServer, getServerPort, reddit } from '@devvit/web/server';
-import type { UiResponse } from '@devvit/web/shared';
+import { createServer, getServerPort } from '@devvit/web/server';
 import { api } from './routes/api';
+import { menu } from './routes/menu';
 
 const app = new Hono();
 const internal = new Hono();
 
-
 app.route('/api', api);
+internal.route('/menu', menu);
 app.route('/internal', internal);
-const menu = new Hono();
-export const createPost = async () => {
-  return await reddit.submitCustomPost({
-    title: 'texthelpermd',
-  });
-};
 
-menu.post('/post-create', async (c) => {
-  try {
-    const post = await createPost();
-
-    return c.json<UiResponse>(
-      {
-        navigateTo: `https://reddit.com/r/${context.subredditName}/comments/${post.id}`,
-      },
-      200,
-    );
-  } catch (error) {
-    console.error(`Error creating post: ${error}`);
-    return c.json<UiResponse>(
-      {
-        showToast: 'Failed to create post',
-      },
-      400,
-    );
-  }
-});
 serve({
   fetch: app.fetch,
   createServer,
